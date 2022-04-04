46e87c20ac32cdedeb5f5da8484c84070129f7c8
HP <HP@DESKTOP-CFP12K1>
2022-04-04 20:52:01 +0530

    First commit

---
+++ LambdaExp/src/Task.java
@@ -0,0 +1,12 @@
+
+interface Pi
+{
+	public double getPivalue();
+}
+public class Task {
+	public static void main(String[] args) {
+		   Pi i;
+	       i=()->3.14;
+	       System.out.println("value of pi ="+i.getPivalue());
+		}
+}
