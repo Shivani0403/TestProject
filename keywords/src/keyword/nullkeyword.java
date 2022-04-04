8bf98301bb04e168727c41b375c9cb614721ef47
HP <HP@DESKTOP-CFP12K1>
2022-04-04 20:50:58 +0530

    First commit

---
+++ keywords/src/keyword/nullkeyword.java
@@ -0,0 +1,11 @@
+
+package keyword;
+
+public class nullkeyword {
+	static nullkeyword obj; //static and null keyword
+
+	public static void main(String[] args) {
+
+		System.out.println(obj);
+	}
+}
---
+++ keywords/src/keywords/Newkeyword.java
@@ -0,0 +1,12 @@
+package keywords;
+public class Newkeyword {
+	void display()  
+    {  
+        System.out.println("Invoking Method");  
+    }  
+      
+    public static void main(String[] args) {  
+        Newkeyword nk=new Newkeyword();
+        nk.display();
+    }  
+}
