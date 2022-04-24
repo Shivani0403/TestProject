354e256ac0e2fef94a7face834f78a3c396ffd8d
HP <HP@DESKTOP-CFP12K1>
2022-04-24 23:56:37 +0530

    First commit

---
+++ Day7/src/Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+	int noofstudents;
+	int cgpa;
+	String Branch;
+	void display()
+	{
+		System.out.println("welcome students to m12 batch");
+	}
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.display();
+
+	}
+}
