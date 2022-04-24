8ca35ad4b65267ee1252fbcc4ba7b045d9b5c40c
HP <HP@DESKTOP-CFP12K1>
2022-04-24 23:43:13 +0530

    First commit

---
+++ Day1/src/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			int a=50;
+			int b=60;
+			int c=a+b;
+			System.out.println("The sum of a and b is "+c);
+		}
+	}
+
+}
