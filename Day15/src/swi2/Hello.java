e4662db6e9262fc30eb6d4b93cf0019faccbaec8
HP <HP@DESKTOP-CFP12K1>
2022-06-04 20:30:21 +0530

    First Commit

---
+++ Day15/src/swi2/Hello.java
@@ -0,0 +1,26 @@
+
+package swi2;
+//string
+public class Hello {
+	public static void main(String[] args)
+	{
+		String name = "te";
+
+		switch (name.toLowerCase()) 
+		{
+		case "author":
+			System.out.println("Shivani");
+			break;
+		case "team":
+			System.out.println("Team Java Full Stack");
+			break;
+		case "editor":
+			System.out.println("Vishnu & Krishna");
+			break;
+		default:
+			System.out.println("Invalid entry");
+			break;
+	}
+}
+
+}
